f813a9ea0d01f1b1e21b3f52e692bc62841dbe3e
Maria Mikhaylova <mihailova_ms@gkl-kemerovo.ru>
2024-07-12 14:05:06 +0300

    init BD

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 // подключение express
 import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+dotenv.config();
+
 // создаем объект приложения
 const app = express();
+app.use(cors());
+const port = process.env.PORT || 8080;
+
+app.use(express.json());
+
 // определяем обработчик для маршрута "/"
-app.get("/", function(request, response){
-     
-    // отправляем ответ
-    response.send("<h2>Привет Express!</h2>");
+app.get("/", function (request, response) {
+  // отправляем ответ
+  response.send("<h2>Привет Express!</h2>");
 });
-// начинаем прослушивать подключения на 3000 порту
-app.listen(3000);
+
+// начинаем прослушивать подключения на запущенном порту
+app.listen(port, () => {
+  console.log(`server run, ${port} `);
+});
